fe7baa144544422f70fc56ec9c29ee23d3cfaf92
Renars <you@example.com>
2025-04-24 14:42:40 +0300

    testing Laravel routes and views

---
+++ resources/views/test.blade.php
@@ -0,0 +1,3 @@
+<?php echo $variable; ?>
+
+{{ $variable }}
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/test', function () {
+//     // return view('welcome');
+
+// });
+
+// Route::get('/test', function (){
+//     echo "<h1>test</h1>";
+//     $name = "Renārs";
+//     echo $name;
+// });
+Route::get('/test', function (){
+    return view('test', ['variable' => "my variable text value"]);
+});
